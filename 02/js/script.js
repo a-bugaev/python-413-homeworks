f16be4b742d1d0e28381c26b1924a25a8c51e145
a-bugaev <a.bugaev.97@gmail.com>
2025-01-28 19:27:12 +0700

    deleted dublicates

--- 02/js/script.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById("btn_change_corolortheme").addEventListener("click", function() {
-  document.body.classList.toggle("dark-mode");
-  document.getElementById("btn_change_corolortheme").innerHTML = document.body.classList.contains('dark-mode') ?
-    '<img src="./img/light_icon.png" alt="icon of light mode"></img>'
-  :
-    '<img src="./img/dark_icon.png" alt="icon of dark mode"></img>'
-})
