d06a1312ff6906b91b620676455ab4474488e481
a-bugaev <a.bugaev.97@gmail.com>
2025-01-29 17:42:01 +0700

    hw_21: almost done, some bug in main()

--- [21] [PY] Функции. Программа сжатия изображений./py_hw_21.py
+++ [21] [PY] Функции. Программа сжатия изображений./py_hw_21.py
@@ -4,11 +4,14 @@
 Оптимизатор изображений v2.0
 """
 
+import sys
 import os
-from PIL import Image
-import pillow_heif
+from time import sleep
 import pillow_avif
+from PIL import Image
+from pillow_heif import from_pillow as heif_from_pillow
 from tqdm import tqdm
+
 
 ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "JPG", "JPEG"}
 DEFAULT_QUALITY = 40
@@ -16,41 +19,45 @@ DEFAULT_FORMAT = "AVIF"
 AVAILABLE_FORMATS = {"WEBP", "HEIF", "AVIF"}
 
 
-def check_extension(image_path:str, allowed:list = None) -> bool:
-    """ check if file format sapported;
-    also can just check if file has ext from 'allowed' list """
+def check_extension(image_path: str, allowed: list = None) -> bool:
+    """check if file format sapported;
+    also can just check if file has ext from 'allowed' list"""
 
     if not allowed:
         allowed = ALLOWED_EXTENSIONS
     ext = os.path.splitext(image_path)[1][1:].lower()
-    for allowed_ext in allowed:
-        if not ext.lower() in allowed_ext.lower():
-            return False
-    return True
+    for item in allowed:
+        if item.lower() == ext.lower():
+            return True
+
+    return False
 
 
-def get_images_paths(
-    source_path: str, target_exts: list = None) -> list[str]:
-    """ recursively gets all images paths from source_path,
-    checking them for ext """
+def get_images_paths(source_path: str, target_exts: list = None) -> list[str]:
+    """recursively gets all images paths from source_path,
+    checking them for ext"""
 
     if not target_exts:
         target_exts = ALLOWED_EXTENSIONS
+
     images_paths = []
+
     if os.path.isfile(source_path):
-        check_extension(source_path, target_exts)
+        if check_extension(source_path, target_exts):
+            images_paths.append(source_path)
 
     if os.path.isdir(source_path):
-        for root, dirs, files in os.walk(source_path):
-            for file in files:
-                if check_extension(file):
-                    images_paths.append(os.path.join(root, file))
+        for dirpath, dirnames, filenames in os.walk(source_path):
+            for filename in filenames:
+                abspath = os.path.join(dirpath, filename)
+                if check_extension(abspath, target_exts):
+                    images_paths.append(abspath)
 
     return images_paths
 
 
-def get_images_disk_space(images_paths: list[str]) -> float:
-    """ returns total disk space used by images in MB """
+def get_images_disk_space_mb(images_paths: list[str]) -> float:
+    """returns total disk space used by images in MB"""
     _bytes = 0
     for image_path in images_paths:
         _bytes += os.stat(image_path).st_size
@@ -58,10 +65,10 @@ def get_images_disk_space(images_paths: list[str]) -> float:
 
 
 def compress_image(
-    image_path: str, output_format: str = DEFAULT_FORMAT, quality: int = DEFAULT_QUALITY
-):
-    """ compress image with Pillow """
+    image_path: str, output_format: str = DEFAULT_FORMAT, quality: int = DEFAULT_QUALITY):
+    """compress image with Pillow"""
     image = Image.open(image_path)
+    output_format = output_format.upper()
 
     if output_format == "WEBP":
         image.save(
@@ -69,59 +76,75 @@ def compress_image(
         )
 
     if output_format == "HEIF":
-        image.save(f"{os.path.splitext(image_path)[0]}.heif", quality=quality)
+        heif_from_pillow(image).save(
+            f"{os.path.splitext(image_path)[0]}.heif", quality=quality
+        )
 
     if output_format == "AVIF":
         image.save(f"{os.path.splitext(image_path)[0]}.avif", quality=quality)
 
 
 def main() -> None:
-    """ Manages the image processing and progress output,
+    """Manages the image processing and progress output,
     checks user input also"""
     while True:
         source_path = input("Введите путь к изображению или директории: ")
-        if not (os.path.isfile(source_path) or os.path.isdir(source_path)):
-            print("Неверный путь")
-            continue
-        else:
+        if os.path.isfile(source_path) or os.path.isdir(source_path):
             break
 
     while True:
         output_format = input(
-            """Введите формат выходного файла (AVIF, WEBP, HEIF):
-                Нажмите Enter для AVIF по умолчанию"""
+            "Введите формат выходного файла (AVIF, WEBP, HEIF)\n"
+            + "(Нажмите Enter для AVIF по умолчанию): "
         )
-        if output_format not in AVAILABLE_FORMATS:
-            print("Неверный формат")
-            continue
-        else:
+        if not output_format:
+            output_format = DEFAULT_FORMAT.lower()
+            break
+        check_passed = False
+        for _format in AVAILABLE_FORMATS:
+            if output_format.upper() == _format.upper():
+                output_format = _format.lower()
+                check_passed = True
+                break
+        if check_passed:
             break
 
     while True:
         quality = input(
-            """Введите качество (от 1 до 100):
-                Нажмите Enter для 40 по умолчанию"""
+            "Введите качество (от 1 до 100)\n" + "(Нажмите Enter для 40 по умолчанию): "
         )
-        if not quality.isdigit() or int(quality) < 1 or int(quality) > 100:
-            print("Число от 1 до 100")
-            continue
-        else:
+        if not quality:
+            quality = DEFAULT_QUALITY
+            break
+        if quality.isdigit() and int(quality) >= 1 and int(quality) <= 100:
             break
 
-    images_paths = get_images_paths(source_path)
+    images_paths = get_images_paths(source_path=source_path)
 
-    size_before = get_images_disk_space(images_paths)
+    size_before = get_images_disk_space_mb(images_paths)
 
-    for i in tqdm(range(size_before * 1024), desc="Compressing... "):
-        for image_path in images_paths:
-            compress_image(image_path, output_format, int(quality))
-            i += get_images_disk_space([image_path])
+    progress_in_kb = int(0)
+    progressbar = tqdm(
+        desc="Compressing...", unit="KB", total=round(size_before * 1024)
+    )
 
-    size_after = get_images_disk_space(get_images_paths(source_path, [output_format]))
+    for image_path in images_paths:
+        compress_image(image_path, output_format, int(quality))
+        sleep(0.5)
+        progress_in_kb += round(get_images_disk_space_mb([image_path]) * 1024)
+        progressbar.update(progress_in_kb)
+    progressbar.close()
+
+    size_after = get_images_disk_space_mb(
+        get_images_paths(source_path, [output_format])
+    )
 
     print(f"Исходный размер: {size_before:.2f} МБ")
-    print(f"Текущий размер: {size_after:.2f} МБ")
+    print(f"После сжатия: {size_after:.2f} МБ")
     print(f"Разница: {size_before - size_after:.2f} МБ")
     print(
         f"Процент сжатия: {round((size_before - size_after) / size_before * 100, 2)}%"
     )
+
+
+main()
